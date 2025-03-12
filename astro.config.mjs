fa5bb33bba19448785d8ce14307886e9c4c9e44d
t4ichi <taichinoheya020316@icloud.com>
2025-03-12 19:58:37 +0900

    feat: Cloudflareへのデプロイに対応するため、Cloudflareアダプターを設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
@@ -11,4 +13,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  output: "server",
+  adapter: cloudflare(),
 });
