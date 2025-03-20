c41ba5ccea5149fb2fe852756cf61433e00d2ad3
t4ichi <taichinoheya020316@icloud.com>
2025-03-20 15:24:59 +0900

    refactor: 画像パス参照の一貫性を高めるため、RAMEN_PATHを導入し命名規則を統一

--- src/feature/ramen/data/ramen-data.ts
+++ src/feature/ramen/data/ramen-data.ts
@@ -1,34 +1,52 @@
 import type { RamenShop } from "../types/ramen-shop";
 
+const RAMEN_PATH = "/ramen";
+
 export const RAMEN_SHOPS: RamenShop[] = [
-  // テンプレート
-  // {
-  //   name: "",
-  //   visitDate: "",
-  //   imageUrl: "ramen/.webp",
-  //   googleMapsUrl: "",
-  //   // instagramUrl: "",
-  //   taberoguUrl: "",
-  //   notes: "",
-  //   tags: [""],
-  // },
+  {
+    name: "武蔵家 大山店",
+    visitDate: "2025-03-20",
+    imageUrl: `${RAMEN_PATH}/武蔵家 大山店_1.webp`,
+    googleMapsUrl: "https://maps.app.goo.gl/fbDkqPahW4HAYEpMA",
+    taberoguUrl: "https://tabelog.com/tokyo/A1322/A132203/13181073/",
+    notes:
+      "並で味玉とかのトッピングがついてくるのありがたい。ライスも無料でおかわりし放題でコスパいい。",
+    tags: ["ラーメン", "家系"],
+  },
+  {
+    name: "武蔵野アブラ学会 代々木店",
+    visitDate: "2025-03-18",
+    imageUrl: `${RAMEN_PATH}/武蔵野アブラ学会 代々木店_1.webp`,
+    googleMapsUrl: "https://maps.app.goo.gl/DVRZAozVsTxC1iEp7",
+    taberoguUrl: "https://tabelog.com/tokyo/A1304/A130403/13161763/",
+    notes: "めちゃうまだった。しかも安い。",
+    tags: ["油そば"],
+  },
+  {
+    name: "油そば専門店 ぶらぶら 新宿店",
+    visitDate: "2025-03-13",
+    imageUrl: `${RAMEN_PATH}/油そば専門店 ぶらぶら 新宿店_1.webp`,
+    googleMapsUrl: "https://maps.app.goo.gl/DVRZAozVsTxC1iEp7",
+    taberoguUrl: "https://tabelog.com/tokyo/A1304/A130401/13200255/",
+    notes:
+      "トッピングいっぱいあった。並・大・特が無料だったが、大盛りくらいでちょうどよかった",
+    tags: ["油そば"],
+  },
   {
     name: "無敵家",
     visitDate: "2025-03-12",
-    imageUrl: "ramen/無敵家.webp",
+    imageUrl: `${RAMEN_PATH}/無敵家_1.webp`,
     googleMapsUrl: "https://maps.app.goo.gl/wcUDVq2mRwqg4TDN6",
-    // instagramUrl: "",
     taberoguUrl: "https://tabelog.com/tokyo/A1305/A130501/13003927/",
     notes:
       "チャーシューがすごかった。平日14時くらいの雨の日でも20分くらい並んだ。",
-    tags: ["ラーメン"],
+    tags: ["ラーメン", "家系"],
   },
   {
     name: "東京油組総本店 西新宿組",
     visitDate: "2025-03-11",
-    imageUrl: "ramen/東京油組総本店 西新宿組.webp",
+    imageUrl: `${RAMEN_PATH}/東京油組総本店 西新宿組_1.webp`,
     googleMapsUrl: "https://maps.app.goo.gl/Q24g1EoSNPB4rU7XA",
-    // instagramUrl: "",
     taberoguUrl: "https://tabelog.com/tokyo/A1304/A130401/13167280/",
     notes: "大盛り無料で腹一杯食べれた。しかも安い。",
     tags: ["油そば"],
@@ -36,9 +54,8 @@ export const RAMEN_SHOPS: RamenShop[] = [
   {
     name: "麺屋NOBUNAGA",
     visitDate: "2025-03-08",
-    imageUrl: "ramen/麺屋NOBUNAGA.webp",
+    imageUrl: `${RAMEN_PATH}/麺屋NOBUNAGA_1.webp`,
     googleMapsUrl: "https://maps.app.goo.gl/2r4penh6WmT9FLG39",
-    // instagramUrl: "",
     taberoguUrl: "https://tabelog.com/tokyo/A1302/A130202/13304145/",
     notes:
       "多分自分以外全員観光客だった。Googleマップの評価が見たことないレベルで高い。",
@@ -47,9 +64,8 @@ export const RAMEN_SHOPS: RamenShop[] = [
   {
     name: "生パスタ専門店 麦と卵 新宿西口店",
     visitDate: "2025-02-25",
-    imageUrl: "ramen/生パスタ専門店 麦と卵 新宿西口店.webp",
+    imageUrl: `${RAMEN_PATH}/生パスタ専門店 麦と卵 新宿西口店_1.webp`,
     googleMapsUrl: "https://maps.app.goo.gl/6QJERVWtCexgE4fP9",
-    // instagramUrl: "",
     taberoguUrl: "https://tabelog.com/tokyo/A1304/A130401/13264682/",
     notes: "お店が中がオシャレだった。大盛りにしてお腹いっぱい食べた。",
     tags: ["パスタ"],
@@ -57,9 +73,8 @@ export const RAMEN_SHOPS: RamenShop[] = [
   {
     name: "十味や",
     visitDate: "2025-02-21",
-    imageUrl: "ramen/十味や.webp",
+    imageUrl: `${RAMEN_PATH}/十味や_1.webp`,
     googleMapsUrl: "https://maps.app.goo.gl/FUCPWjcwfDGSZPnG7",
-    // instagramUrl: "",
     taberoguUrl: "https://tabelog.com/tokyo/A1304/A130401/13011949/",
     notes: "とおみやって読むらしい。",
     tags: ["ラーメン"],
